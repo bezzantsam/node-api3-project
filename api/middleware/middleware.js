c19ac4fb08158fd7d5fea5e664d0f444d60c9538
Sam Bezzant <sambezzant@Sams-MacBook-Pro.local>
2022-03-16 11:28:14 -0700

    validate

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,3 +1,5 @@
+const User = require('../users/users-model')
+
 function logger(req, res, next) {
   const timestamp = new Date().toLocaleString;
   const method = req.method;
@@ -7,10 +9,20 @@ function logger(req, res, next) {
   console.log("logger middleware");
   next();
 }
-function validateUserId(req, res, next) {
-  console.log("validateUserId middleware");
-  // DO YOUR MAGIC
-  next();
+async function validateUserId(req, res, next) {
+  try{
+     const user = await User.getById(req.params.id)
+     if(!user){
+       res.status(404).json({message: 'no such user',
+      })
+     }else{
+       req.user = user
+       next()
+  }
+} catch(err){
+    res.status(500).json({message: 'problem finding user',
+  })
+  }
 }
 
 function validateUser(req, res, next) {
