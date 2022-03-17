97c9329eebd5c21a8ff68497ab02d6508c1a3cd3
Sam Bezzant <sambezzant@Sams-MacBook-Pro.local>
2022-03-17 01:42:23 -0700

    users-router, mvp

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,94 +1,79 @@
 const express = require('express');
-const res = require('express/lib/response');
-const { 
-validateUserId,
-validateUser,
-validatePost,
-}= require('../middleware/middleware')
-// You will need `users-model.js` and `posts-model.js` both
-// The middleware functions also need to be required
-const User = require('./users-model')
-const Post = require('../posts/posts-model')
+
+const User = require('./users-model');
+const Post = require('../posts/posts-model');
+const { validatePost, validateUser, validateUserId } = require('../middleware/middleware');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
   User.get()
-  .then(users => {
-    res.json(users)
-  })
-  .catch(next)
-});
-
-router.get('/:id',validateUserId,(req, res) => {
-  res.json(req.user)
-
-});
-
-router.post('/', validateUser,validatePost, (req, res, next) => {
- User.insert({name: req.name})
- .then(newUser =>{
-   
-   res.status(201).json(newUser)
- })
- .catch(next)
-});
-
-router.put('/:id', validateUserId, validateUser,(req, res, next) => {
-   User.update(req.params.id, {name: req.name} )
-   .then(rowsChanged => {
-     res.json((rowsChanged) => {
-       return User.getById(req.params.id)
-     })
-     .then(user => {
-     res.json(user)
+    .then(users => {
+      res.status(200).json(users);
     })
-   .catch(next)
+    .catch(next);
 });
 
-router.delete('/:id',validateUserId, async(req, res, next) => {
-  try{
-    await User.remove(req.params.id)
-   res.json(req.user)
+router.get('/:id', validateUserId, async (req, res, next) => {
+  try {
+    const user = await User.getById(req.params.id);
+    res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
+});
 
-  }catch(err) {
-    next(err)
+router.post('/', validateUser, async (req, res, next) => {
+  try {
+    const newUser = await User.insert(req.body);
+    res.status(201).json(newUser);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put('/:id', validateUserId, validateUser, async (req, res, next) => {
+  try {
+    const updated = await User.update(req.params.id, { name: req.name });
+    res.status(200).json(updated);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete('/:id', validateUserId, async (req, res, next) => {
+  try {
+    await User.remove(req.params.id);
+    res.status(200).json(req.user);
+  } catch (err) {
+    next(err);
   }
 });
 
 router.get('/:id/posts', validateUserId, async (req, res, next) => {
-  try{
-  const result = await User.getUserPosts(req.params.id)
-  res.json(result)
-
-  }catch(err) {
-    next(err)
+  try {
+    const posts = await User.getUserPosts(req.params.id);
+    res.status(200).json(posts);
+  } catch (err) {
+    next(err);
   }
-  
 });
 
-router.post(
-  '/:id/posts',
-  validateUserId,
-  validatePost, 
-  async (req, res, next) => {
-   try {
-       await Post.insert({
-        user_id: req.params.id,
-        text: req.text,
-      })
-      res.status(201).json(result)
-    
-   }catch (err){
-     next(err)
-   }
-  
+router.post('/:id/posts', validateUserId, validatePost, async (req, res, next) => {
+  try {
+    const newPost = await Post.insert({ user_id: req.params.id, text: req.text });
+    res.status(201).json(newPost);
+  } catch (err) {
+    next(err);
+  }
 });
-router.use((err, req, res, next) => {
-  res.status(err.status || 500).json({customMessage: 'something tragic inside posts router happened',
+
+router.use((err, req, res, next) => { //eslint-disable-line
+  res.status(err.status || 500).json({
+    customMessage: "Something broke inside users-router",
     message: err.message,
-     stack: err.stack,
-})
-})
-})
-// do not forget to export the router
+    stack: err.stack
+  });
+});
+
 module.exports = router;
